3f615327a6e0f26d157edbd5edfc639d26a4ad19
fobadara <fobadara@gmail.com>
2022-06-18 16:48:45 +0100

    Fix error

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-class Api::MessagesController < ApplicationController
+module Api
+  class MessagesController < ApplicationController
     def index
       greeting = Message.order(Arel.sql('RANDOM()')).first
       render json: { message: greeting.content }
